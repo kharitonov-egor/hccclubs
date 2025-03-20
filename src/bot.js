439cb826f66080a945ab28d51786a9d748bae9a0
Egor Kharitonov <ega.khar@gmail.com>
2025-03-20 14:01:51 -0400

    Changed bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,9 @@
 import { Client, GatewayIntentBits } from 'discord.js';
 
+import { WebSocketManager, WebSocketShardEvents, CompressionMethod } from '@discordjs/ws';
+import { REST } from '@discordjs/rest';
+import { RESTGetAPIGatewayBotResult } from 'discord-api-types/v10';
+
 import { DISCORD_API_KEY, OPENAI_API_KEY } from './APIKEYS.js'
 import OpenAI from "openai";
 
@@ -20,30 +24,51 @@ async function getResponse(userPrompt) {
 // Example usage
 
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-    ],
+const rest = new REST().setToken(DISCORD_API_KEY );
+// This example will spawn Discord's recommended shard count, all under the current process.
+const manager = new WebSocketManager({
+    token: DISCORD_API_KEY ,
+    intents: 0, // for no intents
+    fetchGatewayInformation() {
+        return rest.get(Routes.gatewayBot()) as Promise<RESTGetAPIGatewayBotResult>;
+    },
+    // uncomment if you have zlib-sync installed and want to use compression
+    // compression: CompressionMethod.ZlibSync,
+
+    // alternatively, we support compression using node's native `node:zlib` module:
+    // compression: CompressionMethod.ZlibNative,
 });
 
-client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
+manager.on(WebSocketShardEvents.Dispatch, (event) => {
+
+    const client = new Client({
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent,
+        ],
+    });
+
+    client.once('ready', () => {
+        console.log(`Logged in as ${client.user.tag}`);
+    });
+
+    client.on('messageCreate', (message) => {
+
+
+        if (message.author.bot) return; // Ignore bot messages
+        if (message.channel.name == "announcements") {
+            // console.log(`${message.author.username}: ${message.content}`);
+
+            getResponse(message.content);
+        }
+
+    });
+
+    client.login(DISCORD_API_KEY);
+
 });
 
-client.on('messageCreate', (message) => {
-
-
-    if (message.author.bot) return; // Ignore bot messages
-    if (message.channel.name == "announcements") {
-       // console.log(`${message.author.username}: ${message.content}`);
-
-        getResponse(message.content);
-    }
-
-});
-
-client.login(DISCORD_API_KEY);
+await manager.connect();
 
 
