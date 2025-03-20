26f6bc1ed9628bf998ecdb09f86f5861f0108747
Egor Kharitonov <ega.khar@gmail.com>
2025-03-20 14:04:07 -0400

    Testing new branch

--- src/CollegeEventsCalendar.jsx
+++ src/CollegeEventsCalendar.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
+// testing new branch
+
 import { clubEvents } from './assets/clubEvents.js';
 
 function CollegeEventsCalendar() {
