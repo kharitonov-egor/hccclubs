8eae9c05450f045d98fbea7b24f8095509993516
Egor Kharitonov <ega.khar@gmail.com>
2025-04-16 18:47:01 -0400

    Added April events in database

--- src/assets/clubEvents.js
+++ src/assets/clubEvents.js
@@ -1,85 +1,264 @@
-export const clubEvents = [
+const SGAGeneralBody= [
   {
-    date: new Date(2025, 1, 21),
-    time: "5pm",
-    title: "Board and table top games",
-    club: "SHPE",
-    link: "https://discord.com/channels/1237208303977762906/1237208407124213851/1340024791196762122",
-    location: "Oblivion Park",
-    description: "We’re meeting up at Oblivion Games to play board and table top games, please join us! Bring any card games or board games you want with you!",
+    date: new Date(2025, 2, 27),
+    time: "3:30PM",
+    title: "General Body Meeting",
+    club: "SGA",
+    link: null,
+    location: "DTEC 300",
+    description: "Weekly meetings for students to communicate ideas and stay informed about issues and events on campus",
   },
   {
-    date: new Date(2025, 1, 18),
-    time: "3pm",
-    title: "We are people meeting",
+    date: new Date(2025, 3, 3),
+    time: "3:30PM",
+    title: "General Body Meeting",
+    club: "SGA",
+    link: null,
+    location: "DTEC 300",
+    description: "Weekly meetings for students to communicate ideas and stay informed about issues and events on campus",
+  },
+  {
+    date: new Date(2025, 3, 17),
+    time: "3:30PM",
+    title: "General Body Meeting",
+    club: "SGA",
+    link: null,
+    location: "DTEC 300",
+    description: "Weekly meetings for students to communicate ideas and stay informed about issues and events on campus",
+  },
+  {
+    date: new Date(2025, 3, 24),
+    time: "3:30PM",
+    title: "General Body Meeting",
+    club: "SGA",
+    link: null,
+    location: "DTEC 300",
+    description: "Weekly meetings for students to communicate ideas and stay informed about issues and events on campus",
+  },
+]
+
+const CyberHawks = [
+  {
+    date: new Date(2025, 2, 27),
+    time: "7:30PM",
+    title: "CyberHawks Meeting",
+    club: "CyberHawks",
+    link: "https://discord.gg/3x5NREF7Js",
+    location: "DTEC 465",
+    description: "Every Thursday 7:30PM: meet and solve cyber security questions",
+  },
+  {
+    date: new Date(2025, 3, 3),
+    time: "7:30PM",
+    title: "CyberHawks Meeting",
+    club: "CyberHawks",
+    link: "https://discord.gg/3x5NREF7Js",
+    location: "DTEC 465",
+    description: "Every Thursday 7:30PM: meet and solve cyber security questions",
+  },
+  {
+    date: new Date(2025, 3, 10),
+    time: "7:30PM",
+    title: "CyberHawks Meeting",
+    club: "CyberHawks",
+    link: "https://discord.gg/3x5NREF7Js",
+    location: "DTEC 465",
+    description: "Every Thursday 7:30PM: meet and solve cyber security questions",
+  },
+  {
+    date: new Date(2025, 3, 17),
+    time: "7:30PM",
+    title: "CyberHawks Meeting",
+    club: "CyberHawks",
+    link: "https://discord.gg/3x5NREF7Js",
+    location: "DTEC 465",
+    description: "Every Thursday 7:30PM: meet and solve cyber security questions",
+  },
+  {
+    date: new Date(2025, 3, 24),
+    time: "7:30PM",
+    title: "CyberHawks Meeting",
+    club: "CyberHawks",
+    link: "https://discord.gg/3x5NREF7Js",
+    location: "DTEC 465",
+    description: "Every Thursday 7:30PM: meet and solve cyber security questions",
+  },
+
+
+]
+
+const WeArePeople = [
+
+  {
+    title: 'We Are People meeting',
+    description: 'We are People harbors a safe and inclusive environment for the LGBTQ+ community within HCC',
+    date: new Date(2025, 2, 25),
+    time: '3PM',
+    location: 'DHUM 301',
+    link: 'https://discord.com/channels/1284304662043951268/1284304662849126421/1336386353943937085',
     club: "We are people",
-    link: "https://discord.com/channels/1284304662043951268/1284304662849126421/1336386353943937085",
-    location: "DHUM 301",
-    description: "We are People harbors a safe and inclusive environment for the LGBTQ+ community within HCC."
   },
   {
-    date: new Date(2025, 1, 13),
-    time: "3:30pm",
-    title: "SGA General Body Meeting",
-    club: "Student Government Association",
-    link: "none",
-    location: "DTEC 300",
-    description: "The DM SGA meets to discuss matters relevant to the student body.",
+    title: 'We Are People meeting',
+    description: 'We are People harbors a safe and inclusive environment for the LGBTQ+ community within HCC',
+    date: new Date(2025, 3, 1),
+    time: '3PM',
+    location: 'DHUM 301',
+    link: 'https://discord.com/channels/1284304662043951268/1284304662849126421/1336386353943937085',
+    club: "We are people",
   },
   {
-    date: new Date(2025, 1, 20),
-    time: "3:30pm",
-    title: "SGA General Body Meeting",
-    club: "Student Government Association",
-    link: "none",
-    location: "DTEC 300",
-    description: "The DM SGA meets to discuss matters relevant to the student body.",
+    title: 'We Are People meeting',
+    description: 'We are People harbors a safe and inclusive environment for the LGBTQ+ community within HCC',
+    date: new Date(2025, 3, 15),
+    time: '3PM',
+    location: 'DHUM 301',
+    link: 'https://discord.com/channels/1284304662043951268/1284304662849126421/1336386353943937085',
+    club: "We are people",
   },
   {
-    date: new Date(2025, 1, 27),
-    time: "3:30pm",
-    title: "SGA General Body Meeting",
-    club: "Student Government Association",
-    link: "none",
-    location: "DTEC 300",
-    description: "The DM SGA meets to discuss matters relevant to the student body.",
+    title: 'We Are People meeting',
+    description: 'We are People harbors a safe and inclusive environment for the LGBTQ+ community within HCC',
+    date: new Date(2025, 3, 22),
+    time: '3PM',
+    location: 'DHUM 301',
+    link: 'https://discord.com/channels/1284304662043951268/1284304662849126421/1336386353943937085',
+    club: "We are people",
   },
+
+
+]
+
+const PsychologyClub = [
   {
-    date: new Date(2025, 1, 20),
-    time: "6:30pm",
-    title: "HTML & CSS Workshop",
-    club: "Coding club",
-    link: "https://discord.com/channels/1265138216084963403/1265138217351381115/1341266181121769472",
-    location: "DTEC 426",
-    description: "Come to learn how to make your own personal website and publish it to the internet!",
-  },
-  {
-    date: new Date(2025, 2, 3),
-    time: "1 pm",
-    title: "Chess club meeting",
-    club: "Chess club",
-    link: "none",
-    location: "DSSC 126",
-    description: "",
-  },
-  {
-    date: new Date(2025, 2, 10),
-    time: "1 pm",
-    title: "Chess club meeting",
-    club: "Chess club",
-    link: "none",
-    location: "DSSC 126",
-    description: "",
-  },
-  {
-    date: new Date(2025, 1, 24),
-    time: " 3:30-4:15pm",
-    title: "Psychology club meeting",
-    club: "Psychology club",
-    link: "https://discord.com/channels/1199820150623457350/1280686088784187406/1338569819955662950",
+    date: new Date(2025, 2, 24),
+    time: "3:30-4:45pm",
+    title: "Psychology Club meeting",
+    club: "Psychology Club",
+    link: null,
     location: "DSSC 225",
-    description: "",
+    description: "Psychology Club brings together students interested in the field of psychology to bring awareness of mental health, learn more about the field, as well as enjoy being around like-minded people",
   },
+  {
+    date: new Date(2025, 2, 31),
+    time: "3:30-4:45pm",
+    title: "Psychology Club meeting",
+    club: "Psychology Club",
+    link: null,
+    location: "DSSC 225",
+    description: "Psychology Club brings together students interested in the field of psychology to bring awareness of mental health, learn more about the field, as well as enjoy being around like-minded people",
+  },
+  {
+    date: new Date(2025, 3, 7),
+    time: "3:30-4:45pm",
+    title: "Psychology Club meeting",
+    club: "Psychology Club",
+    link: null,
+    location: "DSSC 225",
+    description: "Psychology Club brings together students interested in the field of psychology to bring awareness of mental health, learn more about the field, as well as enjoy being around like-minded people",
+  },
+  {
+    date: new Date(2025, 3, 14),
+    time: "3:30-4:45pm",
+    title: "Psychology Club meeting",
+    club: "Psychology Club",
+    link: null,
+    location: "DSSC 225",
+    description: "Psychology Club brings together students interested in the field of psychology to bring awareness of mental health, learn more about the field, as well as enjoy being around like-minded people",
+  },
+  {
+    date: new Date(2025, 3, 21),
+    time: "3:30-4:45pm",
+    title: "Psychology Club meeting",
+    club: "Psychology Club",
+    link: null,
+    location: "DSSC 225",
+    description: "Psychology Club brings together students interested in the field of psychology to bring awareness of mental health, learn more about the field, as well as enjoy being around like-minded people",
+  },
+
+]
+
+export const clubEvents = [
+    ...SGAGeneralBody,
+    ...CyberHawks,
+    ...WeArePeople,
+    ...PsychologyClub,
+  {
+    date: new Date(2025, 2, 24),
+    time: "11am - 1pm",
+    title: "RSO Fair",
+    club: "SGA",
+    link: "https://www.instagram.com/p/DHD1pcEStG1/",
+    location: "SGA Patio",
+    description: "Connect with student organizations and community partners to learn how to get involved, both on and off campus",
+  },
+  {
+    date: new Date(2025, 3, 22),
+    time: "11am - 2pm",
+    title: "RSO Fair",
+    club: "SGA",
+    link: null,
+    location: "SGA Patio",
+    description: "Connect with student organizations and community partners to learn how to get involved, both on and off campus",
+  },
+
+  {
+    title: 'Florida Intercollegiate Programming Competition',
+    description: 'A Programming competition for individual students, specific language requirements and conditions apply',
+    date: new Date(2025, 3, 12),
+    time: '12:00 p.m. - 4:45 p.m.',
+    location: 'Dale Mabry campus, specific classroom to be announced',
+    club: "Coding Club",
+    link: "https://discord.com/channels/1265138216084963403/1265138217351381115/1351614057903427705",
+  },
+
+  {
+    title: 'Noches de Ciencias',
+    description: 'STEM themed open house for High Schoolers',
+    date: new Date(2025, 2, 31),
+    time: '5-7pm',
+    location: 'Dale Mabry campus',
+    club: "SHPE",
+    link: "https://discord.com/channels/1237208303977762906/1237208407124213851/1349468890585104454",
+  },
+
+  {
+    title: 'How to run clubs',
+    description: 'Club’s meeting featuring the president and vice president of USF’s IEEE Computer Society. More info coming soon',
+    date: new Date(2025, 3, 3),
+    time: '6:30PM',
+    location: 'Not Specified',
+    link: null,
+    club: "Coding Club",
+  },
+
+  {
+    title: 'Achieving Career Growth Workshop',
+    description: 'Participate in our workshops held throughout the semester to gain valuable knowledge and skill development that will help you prepare for your preferred career path',
+    date: new Date(2025, 3, 9),
+    time: '11am - Noon',
+    location: 'DSTU 202',
+    link: 'https://www.instagram.com/p/DF59wqHBVuE/',
+    club: "DM Career Resource Center",
+  },
+
+  {
+    title: 'Taxes Workshop',
+    description: 'Join our Tax Workshop designed specifically for international students. Learn about filing requirements, key deadlines, and helpful tips to make the process easier',
+    date: new Date(2025, 2, 28),
+    time: '9:30AM',
+    location: 'DSSC 319',
+    link: 'https://www.instagram.com/p/DHEYeRORSO8/',
+    club: "Center for International Education",
+  },
+
+
+
+
+
+
+
+
 
 
 
